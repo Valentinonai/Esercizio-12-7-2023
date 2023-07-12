b507b2fc3c8b7745a866b366a0101027e95824ab
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-07-12 20:31:10 +0200

    v2.1 Codice semplificato funzioni

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 let inserimento;
-const ul = document.getElementById("startList");
-let cont = 0; //! CONTATORE PER ATTRIBUIRE UN NOME AGLI ELEMENTI
 const addList = (Lista) => {
-  cont += 1;
-  console.log(cont);
+  const ul = document.getElementById("startList");
   Lista.preventDefault();
   inserimento = Lista.target.elements[0]; //?PRENDO IL VALORE DEL CAMPO TESTO INSERITO
-  console.log(inserimento.value);
-
   const form = document.createElement("form");
   const li = document.createElement("li"); //! CREAZIONE ELEMENTI CON TIPI,NOMI E CLASSI
   const check = document.createElement("input");
+  const delate = document.createElement("button");
   check.type = "checkbox";
-  check.name = cont;
   check.addEventListener("change", checked); //? AGGIUNTO EVENTO SELEZIONE CHECKBOX
   check.classList.add("ckeckBoxStyle");
   form.classList.add("formStyle");
-  const delate = document.createElement("button");
   delate.classList.add("buttonStyle");
   delate.addEventListener("click", cancella); //?   AGGIUNTO EVENTO CLICK
-  delate.name = cont;
-  form.name = cont;
   form.appendChild(delate);
   form.appendChild(check); //! SI POTEVA SCRIVERE ul.INNERHTML+= `<li onclick="FUNZION(event)"></li>`
   li.innerText = inserimento.value;
-  li.name = cont;
   form.appendChild(li);
   ul.appendChild(form);
-  console.dir(delate);
   Lista.target.reset();
 };
 
@@ -36,35 +26,15 @@ const cancella = (event) => {
   const form = document.getElementsByTagName("form");
   event.preventDefault();
   event.target.parentElement.remove(); //TODO    VERSIONE PIù VELOCE
-  /*for (let i = 0; i < form.length; i++) {
-    if (form[i].name === event.target.name) {
-      form[i].remove();
-      console.log(form[i]);
-    }
-  }*/
 };
 //!FUNZIONE CHE DEPENNA UNA RIGA DELLA LISTA
 const checked = (event) => {
-  //const li = document.getElementsByTagName("li");
   event.preventDefault();
-  const x = event.target.closest("form");
+  const x = event.target.parentElement;
+  const li = x.lastChild;
   if (event.target.checked === true) {
-    /* for (let i = 0; i < li.length; i++) {
-      const nome = li[i].name;
-      const N = parseInt(event.target.name);
-      if (nome === N) {
-        li[i].style.textDecoration = "line-through";
-      }
-    }*/
-    x.style.textDecoration = "line-through";
+    li.style.textDecoration = "line-through";
   } else {
-    /*for (let i = 0; i < li.length; i++) {
-      const nome = li[i].name;
-      const N = parseInt(event.target.name);
-      if (nome === N) {
-        li[i].style.textDecoration = "none";
-      }
-    }*/
-    x.style.textDecoration = "none";
+    li.style.textDecoration = "none";
   }
 };
